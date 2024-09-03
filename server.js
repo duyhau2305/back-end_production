ef9863169458df72238a52501bde47fdb7898fc9
duyhau <duyhau2305@gmail.com>
2024-09-04 01:01:22 +0700

    setup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
-const connectDB = require('../config/db');
-const userRoutes = require('../routes/userRoutes');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 const dotenv = require('dotenv');
 const cors = require('cors');
