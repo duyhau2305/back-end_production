51529dd01288b607b72523944081ed306b1a6810
unknown <duyhau2305@gmail.com>
2024-09-30 17:24:48 +0700

    cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const axios = require('axios');
 const connectDB = require('./config/db'); // Kết nối đến MongoDB
 const DeviceStatus = require('./models/DeviceStatus');
+const cors = require('cors')
 const dotenv = require('dotenv');
 const deviceStatusRoute = require('./routes/deviceStatusRoute'); // Import route mới tạo
 
@@ -10,6 +11,7 @@ dotenv.config(); // Tải các biến môi trường từ tệp .env
 
 const app = express();
 const PORT = process.env.PORT || 3000; // Cổng mà server sẽ chạy
+app.use(cors())
 
 // Kết nối đến MongoDB
 connectDB();
