b4e0b6c8e9f43f3c41694bc585e414e8a8a3f477
unknown <duyhau2305@gmail.com>
2024-10-07 16:41:14 +0700

    set up

--- routes/areaRouter.js
+++ routes/areaRouter.js
@@ -6,16 +6,16 @@ const router = express.Router();
 // Create a new area
 router.post('/', areaController.createArea);
 
-// Get all areas
+
 router.get('/', areaController.getAllAreas);
 
-// Get area by ID
+
 router.get('/:id', areaController.getAreaById);
 
-// Update an area by ID
+
 router.put('/:id', areaController.updateArea);
 
-// Delete an area by ID
+
 router.delete('/:id', areaController.deleteArea);
 
 module.exports = router;
