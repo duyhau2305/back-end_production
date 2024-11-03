3781f667c2c3af4b67aef9e40e0adae306abf654
unknown <bao.nguyenhuu1999@gmail.com>
2024-11-03 16:17:09 +0700

    Update deploy

--- config/db.js
+++ config/db.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+        await mongoose.connect(url, {
+            useNewUrlParser: true
         });
         console.log('MongoDB connected...');
     } catch (err) {
