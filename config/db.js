6ab625607fad267770a6ab2465c6a319f404df5e
unknown <bao.nguyenhuu1999@gmail.com>
2024-11-03 16:22:01 +0700

    Remove unuse

--- config/db.js
+++ config/db.js
@@ -6,9 +6,7 @@ dotenv.config();
 const connectDB = async () => {
     try {
         const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-        await mongoose.connect(url, {
-            useNewUrlParser: true
-        });
+        await mongoose.connect(url);
         console.log('MongoDB connected...');
     } catch (err) {
         console.error('MongoDB connection error:', err.message);
